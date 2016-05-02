d4ed91b9472d40073cca43f96a18386f31d34763
Abdulsattar Mohammed <asattar.md@gmail.com>
2016-05-02 19:12:28 +0530

    Alvin/Sattar:Add other source

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,4 +2,12 @@ $(function() {
   $('#copy_address').change(function() {
     $('#user_perm_address').val($('#user_curr_address').val());
   });
+  $('input[name="user[source]"]').change(function() {
+    if($('input[name="user[source]"]:checked').val() === 'Other') {
+      $('#user_source').show();
+    } else {
+      $('#user_source').hide();
+    }
+  });
+  $('input[name="user[source]"]').change();
 });
